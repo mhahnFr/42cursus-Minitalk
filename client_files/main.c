85e38de23eb87291c1dfc1dbbb9d888928fd6586
mhahn <mhahn@student.42heilbronn.de>
2021-11-23 23:01:05 +0100

    ii

--- client_files/main.c
+++ client_files/main.c
@@ -4,9 +4,32 @@
 
 #include "ft_printf.h"
 
+int	pid;
+
+void	send_message(int pid, char *mesg)
+{
+	ft_printf("%d %s\n", pid, mesg);
+}
+
+void	send_pid(int pid, int data)
+{
+	for (size_t i = 0; i < 32; i++) {
+		if (((data >> i) & 0xFF) == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(50);
+	}
+}
+
+void	receive_answer()
+{
+	kill(pid, SIGUSR2);
+}
+
 int	main(int argv, char **argc)
 {
-	int	pid;
+	int	i;
 
 	if (argv <= 2)
 	{
@@ -14,7 +37,16 @@ int	main(int argv, char **argc)
 		return (1);
 	}
 	pid = atoi(argc[1]);
-	kill(pid, SIGUSR1);
-	kill(getpid(), SIGKILL);
+	// send pid
+	// once server got it, he will send a signal
+	// send a bit, send the next bit once signal from server received.
+	signal(SIGUSR1, receive_answer);
+	send_pid(pid, getpid());
+	i = 0;
+	while (i < argv - 1)
+	{
+		send_message(pid, argc[i]);
+		i++;
+	}
 	return (0);
 }
--- server_files/main.c
+++ server_files/main.c
@@ -1,10 +1,36 @@
 #include <unistd.h>
+#include <signal.h>
 
 #include "ft_printf.h"
 
+int	other_pid;
+
+void	receive_zero()
+{
+	write(1, "0", 1);
+
+//	kill(other_pid, SIGUSR1);
+}
+
+void	receive_one()
+{
+	write(1, "1", 1);
+//	kill(other_pid, SIGUSR1);
+}
+
+void	print_it()
+{
+	ft_printf("%d\n", other_pid);
+}
+
 int	main(void)
 {
+	other_pid = 1;
 	ft_printf("%d\n", getpid());
+	signal(SIGUSR1, receive_zero);
+	signal(SIGUSR2, receive_one);
+	signal(SIGKILL, print_it);
+	signal(SIGTERM, print_it);
 	while (1);
 	return (0);
 }
